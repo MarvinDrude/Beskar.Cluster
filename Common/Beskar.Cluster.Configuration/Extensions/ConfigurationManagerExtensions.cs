d70bdb8c5d0857f747ead9993057dcace86fd48b
Marvin Drude <marvindrude1@gmail.com>
2026-04-25 16:20:29 +0200

    Add configuration management module and integrate with logging runner

---
+++ Common/Beskar.Cluster.Configuration/Extensions/ConfigurationManagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Beskar.Cluster.Configuration.Extensions;
+
+public static class ConfigurationManagerExtensions
+{
+   extension(ConfigurationManager config)
+   {
+      public void SetupBeskarClusterConfiguration(string[] args)
+      {
+         config.Sources.Clear();
+         
+         config.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .AddJsonFile("appsettings.User.json", optional: true, reloadOnChange: true);
+      }
+   }
+}
--- Logging/Beskar.Cluster.Logging.Module.Runner/Program.cs
+++ Logging/Beskar.Cluster.Logging.Module.Runner/Program.cs
@@ -1,7 +1,10 @@
+using Beskar.Cluster.Configuration.Extensions;
 using Beskar.Cluster.Logging.Module.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddBeskarClusterServerLogging();
+builder.Configuration.SetupBeskarClusterConfiguration(args);
 
 var app = builder.Build();
 app.UseHttpsRedirection();
